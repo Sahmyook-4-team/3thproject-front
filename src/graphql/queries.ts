ae79b1e6dfbe27e9b3d9ffa3c24f9e2515e99bb2
ZaRi <rbxo0913@gmail.com>
2025-08-28 15:33:37 +0900

    Update queries.ts

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -33,3 +33,34 @@ export const SEARCH_PATIENTS = gql`
     }
   }
 `;
+
+
+
+
+
+// 환자 ID로 환자 정보와 모든 검사(Study) 목록을 가져오는 쿼리
+export const GET_PATIENT_WITH_STUDIES = gql`
+  query GetPatientWithStudies($pid: ID!) {
+    patient(pid: $pid) {
+      pid
+      pname
+      studies {
+        studyKey
+        studydesc
+        studydate
+        studytime
+        modality
+        seriescnt
+        imagecnt
+        report {
+          reportId
+          reportContent
+          reportStatus
+          author {
+            username
+          }
+        }
+      }
+    }
+  }
+`;
