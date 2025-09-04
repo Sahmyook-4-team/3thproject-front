7918190b8f97c2c1bd9fa5fdf776edd74d8c8be7
ZaRi <rbxo0913@gmail.com>
2025-09-05 00:29:46 +0900

    배포 버그 수정

--- next.config.js
+++ next.config.js
@@ -14,6 +14,21 @@ const nextConfig = {
   },
   // 만약 reactStrictMode 같은 다른 설정이 있었다면 여기에 추가하세요.
   // 예: reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      // 규칙 1: /api/ 로 시작하는 모든 REST API 요청을 위한 길
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+      },
+      // 규칙 2: /graphql 로 들어오는 모든 GraphQL 요청을 위한 길
+      {
+        source: '/graphql',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/graphql`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
